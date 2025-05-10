2d3f97ccb907ddd9812ed7b316cc9a1ae74b00c1
‪Nour Eldin Mohamed‬‏ <159364235+noureldinmohamed1@users.noreply.github.com>
2025-05-10 15:38:22 +0300

    add comments and read input from file

--- Problem3.cpp
+++ Problem3.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
+#include "fstream"
 using namespace std;
 
+// Create a vector class
 class Vector
 {
+// initialization of the private attributes
 private:
     int size , ind;
     int* arr;
+// initialization of the public methods
 public:
+    // default constructor
     Vector()
     {
         size = 0 , ind = 0;
         arr = new int[size];
     }
+    // parameterized constructor
     Vector(int siz , int init = 0)
     {
         size = siz;
         ind = 0;
         arr = new int[size];
+        // fill the array with the given value
         Fill(init);
     }
+    // push_back method to add an element to the end of the vector
     void Push_Back (int element)
     {
+        // if the index is greater than or equal to the size of the array
         if (ind >= size)
         {
+            // if the size is 0, create a new array of size 1
             if (!size)
             {
                 size++;
                 delete[] arr;
                 arr = new int[size];
             }
+            // if the size is not 0, double the size of the array
             else
             {
                 size*=2;
@@ -40,9 +51,11 @@ public:
                 arr = newArr;
             }
         }
+        // add the element to the end of the array and increment the index
         arr[ind++] = element;
     }
 
+    // fill method to fill the array with a given value
     void Fill(int val)
     {
         for (int i = 0; i < Capacity(); ++i)
@@ -51,19 +64,25 @@ public:
         }
     }
 
+    // operator overloading to access the elements of the array using []
     int& operator [](int index)
     {
         return arr[index];
     }
 
+    // size method to return the number of elements in the array
     int Size()
     {
         return ind;
     }
+
+    // capacity method to return the size of the array
     int Capacity()
     {
         return size;
     }
+
+    // destructor to delete the array
     ~Vector()
     {
         delete[] arr;
@@ -71,20 +90,43 @@ public:
 };
 
 int main() {
-    cout << "Enter the length of the array :" <<endl;
-    int len; cin >> len;
+    // ask the user to enter the name of the file
+    cout << "Enter the name of the file : " <<endl;
+    string filename; cin >> filename;
+    fstream file(filename);
 
-    Vector v;
-    for (int i = 0; i < len; ++i) {
-        cout << "Enter the #" << i+1 << " element :" <<endl;
-        int val; cin >> val;
-        v.Push_Back(val);
+    // check if the file is open
+    if (file.is_open())
+    {
+        cout << "file opened successfully" <<endl;
+    }
+    else
+    {
+        cout << "file not opened" <<endl;
+        return 0;
     }
 
-    cout << "Enter k :" <<endl;
-    int k; cin >> k;
+    // read the number of test cases from the file
+    int t; file >> t;
+    while (t--)
+    {
+        // read the length of the vector from the file
+        int len; file >> len;
 
-    int ans = 0;
+        // create a vector
+        Vector v;
+
+        // read the elements of the vector from the file
+        for (int i = 0; i < len; ++i) {
+            int val; file >> val;
+            v.Push_Back(val);
+        }
+
+        // read the value of k from the file
+        int k; file >> k;
+
+        // initialize the answer to 0
+        int ans = 0;
 
 //    O(n*k) solution
 //    for (int i = 0; i < len; ++i) {
@@ -106,27 +148,35 @@ int main() {
 
 
 
-//    O(n) solution
-    int lstFlip = 0;
-    Vector freq(len+1);
+        // O(n) solution
 
-    for (int i = 0; i < len; ++i) {
-        lstFlip+=freq[i];
-        if (lstFlip&1)
-            v[i] = 1 - v[i];
-        if (!v[i])
-        {
-            if (i+k > len)
+        // initialize the last flip to 0
+        int lstFlip = 0;
+        // create a frequency vector of size len+1 and initialize it to 0
+        Vector freq(len+1);
+
+        for (int i = 0; i < len; ++i) {
+            lstFlip+=freq[i];
+            // if the last flip is odd, flip the current element
+            if (lstFlip&1)
+                v[i] = 1 - v[i];
+            // if the current element is 0
+            if (!v[i])
             {
-                cout << -1 <<endl;
-                return 0;
+                // if i+k > len, set ans to -1 and break (there is no solution)
+                if (i+k > len)
+                {
+                    ans = -1;
+                    break;
+                }
+                // flip the current element and the next k-1 elements, increment ans and lstFlip
+                ans++ , lstFlip++ , freq[i+k]--;
             }
-            ans++ , lstFlip++ , freq[i+k]--;
         }
-
+        // print ans
+        cout << ans <<endl;
     }
 
-    cout << ans <<endl;
     return 0;
 }
 
