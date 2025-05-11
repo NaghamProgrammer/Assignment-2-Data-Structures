d7898d8f40de9798a17f09cf7d1d4eff9f4360c9
Aly-El-Badry <ali.el.badry.747@gmail.com>
2025-05-12 01:09:14 +0300

    adding code of node

--- Problem2.cpp
+++ Problem2.cpp
@@ -1,5 +1,65 @@
 #include "iostream"
 using namespace std;
 
+struct Contact {
+    string name;
+    string phone;
+    string email;
+};
+
+class Node {
+private:
+    int key;
+    int height;
+    Contact Value;
+    Node* left;
+    Node* right;
+public:
+    Node(const int &id,const Contact &value) {
+        int height=1;
+        this->key = id;
+        this->Value = value;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+    int getKey() const {
+        return key;
+    }
+
+    void setKey(int key) {
+        this->key = key;
+    }
+
+    Contact getValue() const {
+        return Value;
+    }
+
+    void setValue(const Contact &value) {
+        Value = value;
+    }
+
+    Node *getLeft() const {
+        return left;
+    }
+
+    void setLeft(Node *left) {
+        this->left = left;
+    }
+
+    Node *getRight() const {
+        return right;
+    }
+
+    void setRight(Node *right) {
+        this->right = right;
+    }
+    int getHeight() const {
+        return height;
+    }
+    void setHeight(const int & height) {
+        this->height = height;
+    };
+};
+
 int main() {
 }
